f05069aa58c50e8ea14977cdf8070b4135bd3b6f
unknown <ghernandezhack@gmail.com>
2020-05-29 16:15:40 -0600

    Correcion y mejoras de Javascript y Css

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const sizes = document.querySelectorAll('.size');
+const colors = document.querySelectorAll('.color');
 
 function changeSize(){
     sizes.forEach(size =>size.classList.remove('active'));
     this.classList.add('active');
 }
 
+function changeColor(){
+    let primary =this.getAttribute('primary');
+    colors.forEach(cl =>cl.classList.remove('active'));
+    this.classList.add('active');
+
+    document.documentElement.style.setProperty('--primary', primary);
+}
+
 sizes.forEach(size => size.addEventListener('click', changeSize));
+colors.forEach(cl => cl.addEventListener('click',changeColor));
