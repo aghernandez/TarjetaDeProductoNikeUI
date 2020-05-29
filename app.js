6e95dc6c0831084e6769b958e148838ed28632f9
unknown <ghernandezhack@gmail.com>
2020-05-29 17:09:14 -0600

    Agregando funcionalidad al backgroud

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const sizes = document.querySelectorAll('.size');
 const colors = document.querySelectorAll('.color');
+const shoes = document.querySelectorAll('.shoe'); 
+const gradient = document.querySelectorAll('gradient');
 
 function changeSize(){
     sizes.forEach(size =>size.classList.remove('active'));
@@ -8,10 +10,16 @@ function changeSize(){
 
 function changeColor(){
     let primary =this.getAttribute('primary');
+    let color = this.getAttribute('color');
+    let shoe = document.querySelector(`.shoe[color="${color}"]`);
+
     colors.forEach(cl =>cl.classList.remove('active'));
     this.classList.add('active');
 
     document.documentElement.style.setProperty('--primary', primary);
+
+    shoes.forEach(s => s.classList.remove('show')); 
+    shoe.classList.add('show');
 }
 
 sizes.forEach(size => size.addEventListener('click', changeSize));
