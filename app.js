e6288fea282eebe742976e5c1852d10596ecfd76
unknown <ghernandezhack@gmail.com>
2020-05-29 19:15:40 -0600

    Tarjeta terminada con animaciones

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const sizes = document.querySelectorAll('.size');
 const colors = document.querySelectorAll('.color');
 const shoes = document.querySelectorAll('.shoe'); 
-const gradient = document.querySelectorAll('gradient');
+const gradients = document.querySelectorAll('.gradient');
+const shoeBg = document.querySelector('.shoeBaackground');
+
+let prevColor="blue"; 
+let animationEnd = true; 
 
 function changeSize(){
     sizes.forEach(size =>size.classList.remove('active'));
@@ -9,9 +13,14 @@ function changeSize(){
 }
 
 function changeColor(){
+    if(!animationEnd) return; 
     let primary =this.getAttribute('primary');
     let color = this.getAttribute('color');
     let shoe = document.querySelector(`.shoe[color="${color}"]`);
+    let gradient = document.querySelector(`.gradient[color="${color}"]`);
+    let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
+
+    if(color == prevColor) return; 
 
     colors.forEach(cl =>cl.classList.remove('active'));
     this.classList.add('active');
@@ -20,7 +29,34 @@ function changeColor(){
 
     shoes.forEach(s => s.classList.remove('show')); 
     shoe.classList.add('show');
+
+    gradients.forEach(g => g.classList.remove('first', 'second'));
+    gradient.classList.add('first');
+    prevGradient.classList.add('second');
+
+    prevColor = color; 
+    animationEnd = false;
+
+    gradient.addEventListener('animationend', () => {
+        animationEnd = true; 
+    })
 }
 
 sizes.forEach(size => size.addEventListener('click', changeSize));
 colors.forEach(cl => cl.addEventListener('click',changeColor));
+
+let x = window.matchMedia("(max-width: 1000px)");
+
+function changeHeight () {
+    if(x.matches){
+        let shoeHeight = shoes[0].offsetHeight;
+        shoeBg.style.height = `${shoeHeight * 0.9}px`; 
+    }
+    else{
+        shoeBg.style.height = "475px";
+    }
+}
+
+changeHeight();
+
+window.addEventListener('resize', changeHeight); 
